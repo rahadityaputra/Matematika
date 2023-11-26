a16b61ae3fcf7db42fa7aa2af787b45bec1d7d70
rahadityaputra <rahadit.a.p123@gmail.com>
2023-11-26 23:49:20 +0700

    tambah atr name , rapikan js kalkulator

--- calculator.js
+++ calculator.js
@@ -1,66 +1,40 @@
-/* menangkap element angka di layar kalkulator agar 
- sesuai dengan keinginan user */
-const angka_layar = document.querySelector('.container .screen span.number');
-
-// menangkap element button angka / operator agar bisa di menghasilkan nilai tapi ini mengembalikan nodelist (seperti array tetapi bukan array)
-// memngubah nodelist button menjadi array; 
-const button = Array.from(document.querySelectorAll('.container .button div'));
-
-
-function tambah(angka1, angka2) {
-    return angka1 + angka2;
-}
-function kurang(angka1, angka2) {
-    return angka1 - angka2;
-}
-function kali(angka1, angka2) {
-    return angka1 * angka2;
-}
-function bagi(angka1, angka2) {
-    return (angka1 / angka2).toFixed(4); // karena supaya angka dibelakang koma di batasi
-}
-
-// membuat vaiabel angka pertama dan angka kedua
 let angka1;
 let angka2;
 let operator;
-
-
-for (let i = 0; i < button.length; i++) {
-    // jika button itu menerima kejadian klik maka jalankan functionnya
-    button[i].addEventListener('click', function () {
-        if (this.getAttribute('name') != 'operator' && this.getAttribute('name') != 'sama-dengan' && this.getAttribute('name') != 'hapus' ) {
-            if (angka_layar.textContent.length < 6) // jika isi angka_layar kurang dari 6 digit     
-                angka_layar.innerHTML += this.getAttribute('class'); // maka bisa ditambah angkanya.
-        }
-    } )    
-}
-
-
-
-// jika operator diklik
-const button_operator = document.querySelectorAll('.button div[name="operator"]');
-for (let j = 0; j < button_operator.length; j++) {
-    button_operator[j].addEventListener('click', function () {
-        if (this.getAttribute('class') == '-' && angka_layar.textContent == '') {
-                angka_layar.innerHTML += '-';
-        } else {
-            angka1 = parseInt(angka_layar.textContent);
-            angka_layar.innerHTML = '';
-            // angka_layar.innerHTML = this.getAttribute('class'); 
-            operator = this.getAttribute('class');
-        }
-        
-    })
-}
-
-
-// jika tombol sama dengan diklik 
+const angka_layar = document.querySelector('.container .screen span.number');
 const button_sama_dengan = document.querySelector('.button .sama-dengan'); 
-button_sama_dengan.addEventListener('click', function () {
-    // kita masukkan input angka ke dua kedalam variabel
-    angka2 = parseInt(angka_layar.textContent);
+const button_clear = document.querySelector('.button .clear'); 
+const button_angka = Array.from(document.querySelectorAll('.container .button div[name="angka"]'));
+const button_operator = Array.from(document.querySelectorAll('.button div[name="operator"]'));
 
+function input_angka() {
+    if (angka_layar.textContent.length < 6) {
+        if (angka_layar.textContent == operator) {
+            angka_layar.innerHTML = '';
+        } 
+            angka_layar.innerHTML += this.getAttribute('class');         
+    }
+}
+
+function input_operator() {
+    if (this.getAttribute('class') == '-' && angka_layar.textContent == '') {
+        angka_layar.innerHTML += '-';
+    } else {
+        angka1 = Number(angka_layar.textContent);
+        angka_layar.innerHTML = this.getAttribute('class');
+        operator = this.getAttribute('class');
+    }
+}
+
+function clear() {
+    angka1 = undefined;
+    angka2 = undefined;
+    operator = undefined;
+    angka_layar.innerHTML = '';
+}
+
+function hasil(){
+    angka2 = Number(angka_layar.textContent);
     if (operator == '+') {
         angka_layar.innerHTML = tambah(angka1,angka2);
     } else if (operator == '-') {
@@ -69,13 +43,27 @@ button_sama_dengan.addEventListener('click', function () {
         angka_layar.innerHTML = kali(angka1, angka2);
     } else if (operator == '/') {
         angka_layar.innerHTML = bagi(angka1, angka2);
-        
     }
-})
+}
 
-document.querySelector('.button .clear').addEventListener('click', function () {
-    angka1 = undefined;
-    angka2 = undefined;
-    operator = undefined;
-    angka_layar.innerHTML = '';
-});
+function tambah(angka1, angka2) {
+    return angka1 + angka2;
+}
+
+function kurang(angka1, angka2) {
+    return angka1 - angka2;
+}
+
+function kali(angka1, angka2) {
+    return angka1 * angka2;
+}
+
+function bagi(angka1, angka2) {
+    return (angka1 / angka2).toFixed(4); // karena supaya angka dibelakang koma di batasi
+}
+
+
+for (let i = 0; i < button_angka.length; i++) { button_angka[i].addEventListener('click',input_angka); }
+for (let j = 0; j < button_operator.length; j++) { button_operator[j].addEventListener('click', input_operator); }
+button_sama_dengan.addEventListener('click', hasil);
+button_clear.addEventListener('click', clear);
