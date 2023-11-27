49d95775c42a5632369f633d53cf201e3c025953
rahadityaputra <rahadit.a.p123@gmail.com>
2023-11-27 22:51:53 +0700

    memperbaiki to do list

--- todolist.js
+++ todolist.js
@@ -1,30 +1,58 @@
- // menangkap element ul.
- const ul = document.querySelector('ul');
- // menangkap element button
- const button = document.querySelector('button'); 
- // menangkap element input
- const input = document.querySelector('input'); 
- // membuat events , jika button diklik maka Daftar List akan bertambah.
- button.addEventListener('click', function(){
-     // membuat list baru dengan di dalamnya berisi inputan user
-     const liBaru = document.createElement('li');
-     liBaru.textContent = input.value + ' ';
-     ul.appendChild(liBaru)
+const button_tambah_list = document.querySelector('button'); 
+const input = document.querySelector('input'); 
+const daftar_list = document.querySelector('.container .daftar-list');
+const list = document.getElementsByTagName('li');
 
-     // membuat tombol hapus
-     const hapus = document.createElement('button')
-     hapus.textContent = 'Hapus';
-     liBaru.appendChild(hapus);
-     hapus.addEventListener('click', function(){
-         ul.removeChild(liBaru);
-     })
-     
-     // membuat tombol selesai
-     const selesai = document.createElement('button');
-     selesai.textContent = 'Selesai';
-     liBaru.appendChild(selesai);
-     selesai.addEventListener('click', function(){
-         liBaru.style.textDecoration = 'line-through';
-     })
-     
- })
+ function buatTombol(list_baru) {
+   // membuat tombol hapus
+   const tombol_hapus = document.createElement('button');
+   tombol_hapus.textContent = 'Hapus';
+   list_baru.appendChild(tombol_hapus);
+   tombol_hapus.addEventListener('click', () => hapusList(list_baru));
+   
+   // membuat tombol selesai
+   const tombol_selesai = document.createElement('button');
+   tombol_selesai.textContent = 'Selesai';
+   list_baru.appendChild(tombol_selesai);
+   tombol_selesai.addEventListener('click', () => selesaiList(list_baru));  
+ }
+
+function tambahList() {
+         if (input.value != '' && cekList() ) {
+             const list_baru = document.createElement('li');
+             list_baru.textContent = input.value;
+             daftar_list.appendChild(list_baru);
+             buatTombol(list_baru);
+            } 
+            input.value = '';
+ }
+
+ // funtion untuk menandai list sudah selesai
+ function selesaiList(list) {
+    list.style.textDecoration = 'line-through';
+ }
+
+ // function untuk hapus list 
+ function hapusList(list) {
+    daftar_list.removeChild(list);
+ }
+
+ function cekList() {
+   if (list.length == 0) {
+      return true;
+   } else {
+      let count = 0;
+      for (let i = 0; i < list.length; i++) {
+         if (list[i].firstChild.textContent == input.value) {
+            count++;
+         }
+      }
+      if (count == 0) {
+         return true;
+      } else {
+         return false;
+      }
+   }
+ }
+ // jika button diklik maka akan menjalankan function tambahlist
+ button_tambah_list.addEventListener('click', tambahList);
