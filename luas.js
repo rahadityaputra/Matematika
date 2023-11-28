985f403b560c11c8d33117ed0731e69ae62da195
rahadityaputra <rahadit.a.p123@gmail.com>
2023-11-28 12:53:11 +0700

    mulai mengerjakan program luas

---
+++ luas.js
@@ -0,0 +1,13 @@
+function getLuasPersegi(sisi) {
+    return sisi * sisi;
+}
+
+function getLuasSegitiga(alas, tinggi) {
+    let luas = (alas * tinggi) / 2;
+    return luas;
+}
+
+function getLuasLingkaran(radius) {
+    const phi = 3.14;
+    return phi * radius * radius;
+}
