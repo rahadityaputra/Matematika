dc25a213b1b91507ee857c4da3ffe086dcd1280a
rahadityaputra <rahadit.a.p123@gmail.com>
2023-11-28 20:31:13 +0700

    memperbaiki penulisan css , script yang terhubung index.html

--- src/script.js
+++ src/script.js
@@ -1,4 +1,6 @@
-// ini untuk membuat tampilan waktu real time di website.
+const waktu = document.querySelector('.container .main .waktu span');
+const quote = document.querySelector('.container .main .quote span');
+const kataUcapan = document.querySelector('.container .main .ucapan span');
 
 let day = (new Date().getDay() == 1)? 'Senin' 
             : (new Date().getDay() == 2)? 'Selasa' 
@@ -8,19 +10,19 @@ let day = (new Date().getDay() == 1)? 'Senin'
             : (new Date().getDay() == 6) ? 'Sabtu' 
             : 'minggu';
 
-const time = document.querySelector('.topbar .time span');
 
 let ucapan = (new Date().getHours() <= 10) ? 'Selamat Pagi' 
             : (new Date().getHours() <= 14) ? 'Selamat Siang'
             : (new Date().getHours() <= 18) ? 'Selamat Sore'
             : 'Selamat Malam';
             
-setInterval(function () {
-    time.innerHTML = `${ucapan} ${day}, ${new Date().getHours()} : ${new Date().getMinutes()} : ${new Date().getSeconds()}`;
-}, 1000);
+function getRealTime() {
+    kataUcapan.innerHTML = `${ucapan}`;
+    waktu.innerHTML = `${day}, ${new Date().getHours()}:${new Date().getMinutes()}`;
+}
 
+setInterval(getRealTime, 1000);
 
-// animasi quote
 const kalimat = [`Berusahalah untuk tidak menjadi manusia yang berhasil tapi berusahalah menjadi manusia yang berguna.`,
                 `Nalar hanya akan membawa anda dari A menuju B, namun imajinasi mampu membawa anda dari A ke manapun.`,
                 `Seseorang yang tidak pernah membuat kesalahan sebenarnya tak pernah mencoba sesuatu yang baru.`,
@@ -30,21 +32,14 @@ const kalimat = [`Berusahalah untuk tidak menjadi manusia yang berhasil tapi ber
                 `Jika A adalah kesuksesan hidup, maka A = x + y + z. x adalah bekerja; y adalah bermain; dan z adalah tutup mulutmu.`,
                 `Sumber pengetahuan utama adalah pengalaman.`];
 
+let angkaRandom = parseInt(Math.random() * 100) % kalimat.length;  // math.random untuk menghasilkan angka acak
 
-// membuat angka random untuk kmengacak kalimat quote
-let angka_random = parseInt(Math.random() * 100) % kalimat.length;  
-//menangkap element html untuk wadah kalimat quote 
-const quote = document.querySelector('.container .main .quote span');
-
-
-// jalankan function nya setiap 0,1 detik
 let count= 0;
 const animasi = setInterval(() => {
-    quote.innerHTML += kalimat[angka_random][count];
+    quote.innerHTML += kalimat[angkaRandom][count];
     count++;
-    if (count == kalimat[angka_random].length) {
-        clearInterval(animasi);
+    if (count == kalimat[angkaRandom].length) {
+        clearInterval(animasi); // untuk menghentikan setInterval
     }
-
 }, 100);
 
